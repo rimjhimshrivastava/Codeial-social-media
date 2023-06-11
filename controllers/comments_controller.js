37b4afe08365a24954b37b6b0687dc6d8863be17
uwu_rimjhim <rimjhimshrivastava19@gmail.com>
2023-06-11 23:21:52 +0530

    v0.1.4: Added Parallel jobs queue

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,6 +1,7 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post')
-const commentsMailer = require('../mailers/comments_mailer');
+const queue = require('../config/kue');
+const commentEmailWorker = require('../workers/comment_email_worker');
 module.exports.create = async function(req, res){
     try{
         let post = await Post.findById(req.body.post);
@@ -14,7 +15,14 @@ module.exports.create = async function(req, res){
             post.save();
 
             comment = await comment.populate('user', 'name email');
-            commentsMailer.newComment(comment);
+            //commentsMailer.newComment(comment);
+            let job = queue.create('email', comment).save(function(err){
+                if(err){console.log('*******error in enqueue the newComment', err); return}
+
+                console.log('Job enqueued', job.id);
+            })
+
+
             return res.redirect('back');
         }
 
