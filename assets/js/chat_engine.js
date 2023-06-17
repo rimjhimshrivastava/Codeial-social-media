ba73b5693d6f9f8767d88fea65948b60554e69ac
uwu_rimjhim <rimjhimshrivastava19@gmail.com>
2023-06-17 15:29:34 +0530

    v0.3.1: Added codeial chatroom

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -1,19 +1,67 @@
 //user side or subscriberside
-class ChatEngine{
-    constructor(chatBoxId, userEmail){
+class ChatEngine {
+    constructor(chatBoxId, userEmail, userName) {
         this.chatBox = $(`#${chatBoxId}`);
         this.userEmail = userEmail;
-
+        this.userName = userName;
         this.socket = io.connect('http://localhost:5000');
-        if(this.userEmail){
+        if (this.userEmail) {
             this.connectionHandler();
         }
     }
 
-    connectionHandler(){
+    connectionHandler() {
+        let self = this;
+
+
         //handling event 'connect' on socket
-        this.socket.on('connect', function(){
+        this.socket.on('connect', function () {
             console.log('connection established using sockets')
+            self.socket.emit('join_room', {
+                user_email: self.userEmail,
+                user_name: self.userName,
+                chatroom: 'codeial'
+            })
+        });
+
+        self.socket.on('user_joined', function (data) {
+            console.log('a user joined!', data);
+        });
+
+        //send a message by clicking on the send button
+        $('#send-message').submit(function(e) {
+            e.preventDefault();
+            let msg = $('.message').val();
+            if (msg != '') {
+                $('.message').val('');
+                self.socket.emit('send_message', {
+                    message: msg,
+                    user_email: self.userEmail,
+                    user_name: self.userName,
+                    chatroom: 'codeial'
+                })
+            }
+        })
+        //upon receiving a message
+        self.socket.on('receive_message', function(data){
+            console.log('message received', data.message);
+            let newMessage = $('<li>');
+
+            let messageType = 'friend_text';
+            
+            if(data.user_email == self.userEmail){
+                messageType = 'user_text';
+            };
+
+            newMessage.append($('<span>', {
+                'html': data.user_name
+            }).addClass('sender_name'));
+            newMessage.append($('<span>', {
+                'html': data.message
+            }).addClass('text_message'));
+
+            newMessage.addClass(messageType);
+            $('#chat-messages-list').append(newMessage);
         })
     }
 }
--- config/chat_socket.js
+++ config/chat_socket.js
@@ -1,14 +1,27 @@
 //server side or observer side
-module.exports.chatSockets = function(socketServer){
-    const io = require("socket.io")(socketServer, {
-        //required to be specifiedd after v3
-        cors: {
-          origin: "http://localhost:8000"
-        }
-      });
+module.exports.chatSockets = function (socketServer) {
+  const io = require("socket.io")(socketServer, {
+    //required to be specifiedd after v3
+    cors: {
+      origin: "http://localhost:8000"
+    }
+  });
 
-    io.sockets.on('connection', function(socket){
-        console.log("New connection established on socket.io:", socket.id)
+  io.sockets.on('connection', function (socket) {
+    console.log("New connection established on socket.io:", socket.id)
+
+    socket.on('disconnect', function () {
+      console.log('socket.io disconnected');
+    })
+    socket.on('join_room', function (data) {
+      console.log('joining request recieved', data);
+      socket.join(data.chatroom);
+      io.in(data.chatroom).emit('user_joined', data);
+    })
+
+    //upon receiving a message
+    socket.on('send_message', function (data) {
+      io.in(data.chatroom).emit('receive_message', data);
     });
-
+  });
 }
